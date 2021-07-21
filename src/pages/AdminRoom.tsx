cadeff119f7119ae84cded4297553fd2eb3fd666
jhonscarpa <scarpa.jhonatan@outlook.com>
2021-07-21 19:46:31 -0300

    remove question

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -4,9 +4,10 @@ import { Button } from '../components/Button/index'
 import { RoomCode } from '../components/RoomCode/index'
 import '../style/room.scss'
 // import { useAuth } from '../hooks/useAuths'
-
+import deleteImg from '../assets/images/delete.svg'
 import { Question } from '../components/Question/index'
 import { useRoom } from '../hooks/useRoom'
+import { database } from '../services/firebase'
 
 
 
@@ -24,7 +25,11 @@ export function AdminRoom() {
 
   const {title, questions} = useRoom(roomId)
 
-
+  async function handleDeleteQuestion(questionId:string) {
+  if (window.confirm('Tem certeza que você deseja excluir essa pergunta?')) {
+    const questionRef = await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
+  }
+  }
 
 
 
@@ -57,7 +62,14 @@ export function AdminRoom() {
          key={questions.id}
          content={questions.content}
          author={questions.author}
-         />
+         >
+           <button
+           type="button"
+           onClick={() => handleDeleteQuestion(questions.id)}
+           >
+             <img src={deleteImg} alt="Remover pergunta" />
+           </button>
+         </Question>
          
        )
      })}
